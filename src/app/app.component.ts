ccc8d8ef85c43657e1617cdd8f91a52ade010cb7
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-04-29 23:59:45 +0200

    about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { AtfComponent } from './atf/atf.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, AtfComponent],
+  imports: [HeaderComponent, RouterOutlet, AtfComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
