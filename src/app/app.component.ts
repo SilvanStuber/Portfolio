2fddf066b1f36a245381e80638a498317d7f490c
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-05-09 20:39:28 +0200

    portfolio section headline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { AtfComponent } from './atf/atf.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, AtfComponent, AboutMeComponent, MySkillsComponent],
+  imports: [HeaderComponent, RouterOutlet, AtfComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
