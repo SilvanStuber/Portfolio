5d1c37e12ab7a57fcc6ab39e48ebc84675d15952
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-05-10 18:47:53 +0200

    IMG Portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -14,25 +14,25 @@ export class PortfolioComponent {
       technologies: 'JavaScript | HTML | CSS',
       discription:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-      image: "/assets/img/join.png",
-      linkGitHub: '',
+      image: '/assets/img/join.png',
+      linkGitHub: 'https://github.com/SilvanStuber/join',
       linkApp: '',
     },
-    { 
+    {
       title: 'Sharkie',
       technologies: 'JavaScript | HTML | CSS',
       discription:
         'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
-      image: "/assets/img/sharkie.png",
+      image: '/assets/img/sharkie.png',
       linkGitHub: 'https://github.com/SilvanStuber/Sharkie',
       linkApp: '',
     },
-    { 
+    {
       title: 'Pokédex',
       technologies: 'JavaScript | HTML | CSS | Api',
       discription:
         'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
-      image: "/assets/img/pokedex.png",
+      image: '/assets/img/pokedex.png',
       linkGitHub: 'https://github.com/SilvanStuber/pokedex',
       linkApp: '',
     },
