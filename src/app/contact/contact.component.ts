06370376291bc8e5534c560eb74c272c13f22575
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-05-15 23:20:37 +0200

    Scroll to different areas and atf button inserted, which scrolls back to atf

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  /**
+   * Smoothly scrolls the window to the top.
+   * This function uses the `window.scrollTo` method with a smooth scrolling behavior
+   * to scroll the window to the top (above the fold).
+   */
+  scrollToAtf() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
 }
