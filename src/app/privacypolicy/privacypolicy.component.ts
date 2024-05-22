877fab71ad8d5c3c912807d4f0c3f640d535e998
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2024-05-22 21:58:14 +0200

    privacy policy

--- src/app/privacypolicy/privacypolicy.component.ts
+++ src/app/privacypolicy/privacypolicy.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AppdataService } from '../appdata.service';import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../shared/header/header.component';
+import { MobileComponent } from '../shared/mobile/mobile.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-privacypolicy',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MobileComponent, FooterComponent],
   templateUrl: './privacypolicy.component.html',
   styleUrl: './privacypolicy.component.scss'
 })
 export class PrivacypolicyComponent {
+  appdata = inject(AppdataService);
   constructor(public router: Router) {
     
   }
+  
 }
